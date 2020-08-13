347605b922bfb4c924291116b651df49c9bf28ac
Yani Asari <yani.iman1@gmail.com>
2020-08-13 20:08:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  # fido = Dog.new
+  # fido.name = "Fido"
+  # fido.name
+ 
+  def name
+    dog_name
+  end
+  
+  # def bark 
+  # puts "woof!"
+  # end 
+  
+end 
+
+  
