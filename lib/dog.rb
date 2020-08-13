4151f42e49b37c0c3d6acc6f06f2fd6c9e21062a
Yani Asari <yani.iman1@gmail.com>
2020-08-13 20:10:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @name= dog_name
+  end
+ 
+  def name
+    @name
+  end
+
+end
+ 
+class Dog 
+  def bark
+    puts "woof!"
+  end 
+  
+end 
+
+  
